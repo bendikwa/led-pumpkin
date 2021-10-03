888da400eca428fd46566a2f3d46669de00ce779
Bendik Andreassen <bendik.andreassen@gmail.com>
2021-10-03 14:43:04 +0200

    Add class fir status led

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
-#include <wifi.h>
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
+#include <status_led.h>
+#include <wifi.h>
 // NeoPixel data pin
 #define NEOPIXEL_PIN 5
 // Number of NeoPixeles (Not counting status pixel)
@@ -11,14 +12,22 @@
 #define NEOPIXEL_STATUS_LED 0
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXEL_TOTAL, NEOPIXEL_PIN);
+Status_LED status_led = Status_LED(NEOPIXEL_STATUS_LED, &strip);
+
 int maxBrightness = 100;
 
 void setup() {
   Serial.begin(115200);
   strip.begin();
   strip.clear();
-
+  strip.show();
+  status_led.red();
   start_wifi();
+  status_led.blue();
+  delay(1000);
+  status_led.green();
+  delay(1000);
+  status_led.off();
 }
 
 // the loop function runs over and over again forever
